30d2c9d0d3faec91635f2f36a848560a7afaac4b
MVeerasi <mark.veerasingam@analog.com>
2024-09-18 20:24:27 +0100

    first commit. Setup daisyUI + tailwind with sveltekit

---
+++ battery-simulator/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import "../app.css";
+    import { goto } from "$app/navigation";
+</script>
+
+<!-- Navbar -->
+<div class="bg-base-100 justify-between fixed top-0 left-0 right-0">
+    <div class="navbar max-w-3xl mx-auto justify-between">
+        <!-- left side of navbar -->
+        <div>
+            <a href="/" class="btn btn-ghost text-xl">PokePage</a>
+            <a href="/user" class="btn btn-ghost">My Page</a>
+        </div>
+        <!-- right side of navbar -->
+        <div>
+            <button on:click={() => goto("/login")}>Login</button>
+            <span class="text-black text-lg ml-2">currentUser@gmail.com</span>
+            <button on:click={() => console.log("logigng out")}>Logout</button>
+        </div>
+    </div>
+</div>
+
+<slot />
---
+++ battery-simulator/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!--http://localhost:5173/-->
+
+<div class="hero min-h-screen bg-base-300">
+    <div class="hero-content">
+        <div class="max-2-md text-center">
+            <h1 class="text-black font-bold text-4xl">Welcome to PokePage</h1>
+            <p class="py-6">
+                Create an account to make your own PokePage with your own unique
+                URL!
+            </p>
+            <a href="/login" class="btn btn-primary">Create An Account</a>
+        </div>
+    </div>
+</div>
